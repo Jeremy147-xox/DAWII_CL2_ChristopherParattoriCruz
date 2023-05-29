3e33d079589d1a83a80c8590e19fad035cdaf66e
Jeremy147-xox <jerechris23@outlook.com>
2023-05-29 16:53:26 -0500

    se inicializo el proyecto

---
+++ src/main/java/cibertec/DAWII_CL2_ChristopherParattoriCruz/DawiiCl2ChristopherParattoriCruzzApplication.java
@@ -0,0 +1,13 @@
+package cibertec.DAWII_CL2_ChristopherParattoriCruz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl2ChristopherParattoriCruzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl2ChristopherParattoriCruzzApplication.class, args);
+	}
+
+}
